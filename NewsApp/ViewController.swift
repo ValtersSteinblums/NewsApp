b6d6502d43d26e1285a93f3eb6b8e40e717ef537
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2022-08-24 16:04:18 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by valters.steinblums on 24/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
